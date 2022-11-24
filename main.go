5aa6148bd2c181f6a8cd5e21d2c16c4b1c04c9ef
yakuhito <y@kuhi.to>
2022-11-23 17:43:07 -0800

    new architecture for Leaflet stuff

--- main.go
+++ main.go
@@ -70,6 +70,7 @@ type GiftCodeAttempts struct {
 }
 
 var db *sql.DB;
+var leaflet_base_url string
 
 func getWeekId() string {
     // https://stackoverflow.com/questions/47193649/week-number-based-on-timestamp-with-go
@@ -358,7 +359,6 @@ func getAPIKeyAndWeeklyUsage(api_key string) (*APIKey, *WeeklyUsage) {
     if err == sql.ErrNoRows {
         return nil, nil
     } else if err != nil {
-        log.Print(err)
         return nil, nil
     }
 
@@ -373,7 +373,6 @@ func createWeeklyUsage(api_key string) *WeeklyUsage {
         api_key, 0, week_id,
     )
     if err != nil {
-        log.Print(err)
         return nil
     }
 
@@ -733,7 +732,7 @@ func revokeAPIKeys(
     return nil
 }
 
-func taxTrafficAndReturnOrigin(api_key string, endpoint string, credits_per_request int64) (string /*origin*/, bool /*errored*/) {
+func taxTrafficAndReturnOrigin(api_key string, credits_per_request int64) (string /*origin*/, bool /*errored*/) {
     apiKey, weeklyUsage := getAPIKeyAndWeeklyUsage(api_key)
 
     if weeklyUsage == nil {
@@ -767,48 +766,48 @@ func taxTrafficAndReturnOrigin(api_key string, endpoint string, credits_per_requ
     return apiKey.origin, false
 } 
 
-func leafletHandler(c *fiber.Ctx, api_key string, endpoint string, leaflet_base_url string) error {
-    const CREDITS_PER_REQUEST = 420;
-    origin, errored := taxTrafficAndReturnOrigin(api_key, endpoint, CREDITS_PER_REQUEST)
+func getAPIKeyForRequest(c *fiber.Ctx) string {
+    api_key := c.Params("api_key")
+    if api_key == "" {
+        api_key = c.Get("X-API-Key")
+    }
+    if api_key == "" {
+        api_key = c.Query("api-key")
+    }
+
+    return api_key
+}
+
+func leafletHandler(c *fiber.Ctx) error {
+    api_key := getAPIKeyForRequest(c)
+    if api_key == "" {
+        return c.Status(401).SendString("No API key provided.")
+    }    
+
+    const CREDITS_PER_REQUEST = 420
+    origin, errored := taxTrafficAndReturnOrigin(api_key, CREDITS_PER_REQUEST)
     if errored {
-        return c.SendString("Taxman has blocked this request.")
+        return c.Status(401).SendString("Taxman has blocked this request.")
     }
     c.Set("Access-Control-Allow-Origin", origin)
 
+    endpoint := c.Params("endpoint")
     url := fmt.Sprintf("%s/%s", leaflet_base_url, endpoint)
+
     resp, err := http.Post(url, "application/json", bytes.NewBuffer(c.Body()))
     if err != nil {
         log.Print(err)
-        return c.SendString("Leaflet: error ocurred when processing request")
+        return c.Status(500).SendString("Leaflet: error ocurred when processing request")
     }
     defer resp.Body.Close()
     
     body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
         log.Print(err)
-        return c.SendString("Leaflet: error ocurred when reading response")
+        return c.Status(500).SendString("Leaflet: error ocurred when reading response")
     }
+    c.Set("Content-Type", "application/json")
     return c.SendString(string(body))
-}
-
-func leafletRouteWithAPIKeyHandler(c *fiber.Ctx, leaflet_base_url string) error {
-    api_key := c.Params("api_key")
-    endpoint := c.Params("endpoint")
-
-    c.Set("X-API-Key", api_key)
-    return leafletHandler(c, api_key, endpoint, leaflet_base_url)
-}
-
-func leafletRouteWithoutAPIKeyHandler(c *fiber.Ctx, leaflet_base_url string) error {
-    api_key := c.Query("api-key")
-    if api_key == "" {
-        api_key = c.Get("X-API-Key")
-    } else {
-        c.Set("X-API-Key", api_key)
-    }
-    endpoint := c.Params("endpoint")
-
-    return leafletHandler(c, api_key, endpoint, leaflet_base_url)
 }
 
 func stripeWebhook(c *fiber.Ctx) error {
@@ -1270,7 +1269,7 @@ func main() {
    if leaflet_port == "" {
        leaflet_port = "18444"
    }
-   leaflet_base_url := fmt.Sprintf("http://%s:%s", leaflet_host, leaflet_port)
+   leaflet_base_url = fmt.Sprintf("http://%s:%s", leaflet_host, leaflet_port)
    fmt.Printf("Leaflet at %s\n", leaflet_base_url)
 
 
@@ -1324,19 +1323,10 @@ func main() {
     db.SetConnMaxLifetime(30 * time.Second)
 
     // Leaflet
-    app.Get("/:api_key<guid>/leaflet/:endpoint", func(c *fiber.Ctx) error {
-        return leafletRouteWithAPIKeyHandler(c, leaflet_base_url)
-    })
-    app.Post("/:api_key<guid>/leaflet/:endpoint", func(c *fiber.Ctx) error {
-        return leafletRouteWithAPIKeyHandler(c, leaflet_base_url)
-    })
-
-    app.Get("/leaflet/:endpoint", func(c *fiber.Ctx) error {
-        return leafletRouteWithoutAPIKeyHandler(c, leaflet_base_url)
-    })
-    app.Post("/leaflet/:endpoint", func(c *fiber.Ctx) error {
-        return leafletRouteWithoutAPIKeyHandler(c, leaflet_base_url)
-    })
+    app.Get("/leaflet/:endpoint", leafletHandler)
+    app.Post("/leaflet/:endpoint", leafletHandler)
+    app.Get("/:api_key<guid>/leaflet/:endpoint", leafletHandler)
+    app.Post("/:api_key<guid>/leaflet/:endpoint", leafletHandler)
 
     // Stripe webhook
     stripe_token := os.Getenv("STRIPE_SECRET_KEY")
