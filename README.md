5f71673b3ea221877a1b9533fc58bbcce1d0c790
yakuhito <y@kuhi.to>
2022-11-25 23:08:43 -0800

    security update

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### (formerly: taxman)
 
 ```
-ghcr.io/fireacademy/catchpole:1.0.6
+ghcr.io/fireacademy/catchpole:1.0.7
 ```
--- tax.go
+++ tax.go
@@ -21,6 +21,9 @@ func taxTrafficAndReturnOrigin(api_key string, credits_per_request int64) (strin
     if apiKey.weekly_credit_limit != -1 && weeklyUsage.credits + credits_per_request > apiKey.weekly_credit_limit {
         return "", true
     }
+    if apiKey.weekly_credit_limit == -1 && apiKey.weekly_credit_limit == 0 {
+        return "", true
+    }
 
     err := increaseWeeklyUsage(api_key, credits_per_request)
     if err != nil {
